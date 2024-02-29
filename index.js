35d3fb73ffdd5612a48d3d0143fecdd901e266ff
kannan <133184797+rs-kannan@users.noreply.github.com>
2024-02-29 15:36:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const AdminRouter = require('./Controllers/admincontroller')
-const ProductRouter = require('./Controllers/productcontroller')
-const UserRouter = require('./Controllers/usercontroller')
-const OrderRouter = require('./Controllers/ordercontroller')
-const URL = 'mongodb+srv://root:root@cluster0.cnnzxyr.mongodb.net/'
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const AdminRouter = require('./Controllers/admincontroller');
+const ProductRouter = require('./Controllers/productcontroller');
+const UserRouter = require('./Controllers/usercontroller');
+const OrderRouter = require('./Controllers/ordercontroller');
+const URL = 'mongodb+srv://root:root@cluster0.cnnzxyr.mongodb.net/';
+const port = 5000;
 
-const port = 5000
-
-async function Connect(){
-    try{
-        await mongoose.connect(URL)
-        console.log('Database connection Successfull')
-    }
-    catch{
-        console.log('DB Connection Error')
+async function Connect() {
+    try {
+        await mongoose.connect(URL);
+        console.log('Database connection successful');
+    } catch (error) {
+        console.error('DB Connection Error:', error);
     }
 }
 
 app.listen(port, '0.0.0.0', () => {
-    console.log('Server is Started in the port', port)
-})
+    console.log('Server is started on port', port);
+});
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send({
         data: "API CREATED SUCCESSFULLY!!"
-    })
-})
+    });
+});
 
-app.use(cors())
-app.use(express.json())
-app.use('/', AdminRouter)
-app.use('/', ProductRouter)
-app.use('/',UserRouter)
-app.use('/',OrderRouter)
+app.use(cors());
+app.use(express.json());
+app.use('/', AdminRouter);
+app.use('/', ProductRouter);
+app.use('/', UserRouter);
+app.use('/', OrderRouter);
 
-Connect()
-
-
-
+Connect();
